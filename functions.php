8d10d21fb93b8933990fa6204c832329f54228f6
Joost Blaauwwiekel <keeperjoost@gmail.com>
2020-01-16 13:08:19 +0100

    first commit

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
 	$USER_USERNAME = "user";
+	$USER_PASSWORD = '$argon2i$v=19$m=1024,t=2,p=2$akM3UnRGWWNHNC85bTJLdg$psjf2u6BklW6ZXim+5AEEIPgHoAEBFsreR+ar4mpz6w'; // dikkepoes123!
 
 	function verify_login($username, $password) {
 		global $USER_USERNAME, $USER_PASSWORD; 
