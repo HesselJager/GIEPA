c445eae442c4488aff2a397d99731e85504f8a7a
Joost Blaauwwiekel <keeperjoost@gmail.com>
2020-01-16 14:07:26 +0100

    shows current temperature
    An ajax call gets the most recent temperature. For know its only a random int.

--- main.php
+++ main.php
@@ -1,3 +1,21 @@
+<script type="text/javascript">
+
+function showTemp() {{
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("temperature").innerHTML = this.responseText;
+        }
+    }
+    xmlhttp.open("GET", "ajax_temperature.php", true);
+    xmlhttp.send();
+    }
+}
+
+window.setInterval(function(){
+  	showTemp()
+}, 1000);
+</script>
 <?php
 
 	include 'functions.php';
@@ -9,3 +27,4 @@
 
 ?>
 <a href='logout.php'>Log out</a>
+<div id="temperature"></div>
