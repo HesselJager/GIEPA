a6ad368cf6dbd45664e9ce865b0028bfc46b2a93
Joost Blaawwiekel <keeperjoost@gmail.com>
2020-01-17 11:23:44 +0100

    changed the header

--- main.php
+++ main.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<title>Weather Information</title>
 	    <style>
     	html,
@@ -10,10 +11,7 @@
 body {
   height: 100%;
   background-color: #f5f5f5;
-  width: 99.6%;
-  padding: 0.2%;
-  margin: 0px;
-  border: 0px;
+  margin: 0;
 }
 #content{
 	min-height:90vh;
@@ -92,12 +90,29 @@ window.setInterval(function(){
 
 ?>
 <body>
-<header><img src="https://www.giepa.gm/sites/default/files/logo-giepa.png" alt="" style="height: 80%;
+<!--<header><img src="https://www.giepa.gm/sites/default/files/logo-giepa.png" alt="" style="height: 80%;
 background-color:#f5f5f5 ; border:5px solid #0B6461;">
 <a href='logout.php' style="position: absolute; right: 2%; top:2%; height:4%;"><button>Log out</button></a>
 </header>
+
+<nav class="navbar navbar-light" style="background-color: #DC292A;">--->
+  <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #DC292A; ">
+    <a class="navbar-brand" href="#">
+      <img src="https://www.giepa.gm/sites/default/files/logo-giepa.png" width="120" height="40" alt="">
+    </a>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav" style="font-size: 20px;">
+      <a class="nav-item nav-link" href="#" style="color: #fff;">Home</a>
+      <a class="nav-item nav-link" href="#" style="color: #fff;">Stations</a>
+      <a class="nav-item nav-link" href="logout.php" style="color: #fff;">Log out</a>
+    </div>
+  </div>
+</nav>
 <div id="content"></div>
 <footer></footer>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 
 </html>
