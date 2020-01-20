1ba6132894d72e0c2ee83a30d968da614359475e
Joost Blaauwwiekel <keeperjoost@gmail.com>
2020-01-20 11:14:27 +0100

    added maps.php
    concept for the google maps system

---
+++ maps.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+       /* Set the size of the div element that contains the map */
+      #map {
+        height: 400px;  /* The height is 400 pixels */
+        width: 100%;  /* The width is the width of the web page */
+       }
+    </style>
+  </head>
+  <body>
+    <h3>My Google Maps Demo</h3>
+    <!--The div element for the map -->
+    <div id="map"></div>
+    <script>
+function initMap() {
+  var gambia = {lat: 13.2, lng: -16.633};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: gambia
+  });
+
+  var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '<h2>Select this station</h2>'+
+      '</div>'+
+      '</div>';
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  var marker = new google.maps.Marker({
+    position: gambia,
+    map: map,
+    title: 'Uluru (Ayers Rock)'
+  });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+}
+    </script>
+    <!--Load the API from the specified URL
+    * The async attribute allows the browser to render the page while the API loads
+    * The key parameter will contain your own API key (which is not needed for this tutorial)
+    * The callback parameter executes the initMap() function
+    -->
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDupy91HDLCEYrvsBj32obYqZhbFmg5dPg&callback=initMap">
+    </script>
+  </body>
+</html>
