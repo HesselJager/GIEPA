ad8100ce080638204c67285fa682f04b74a606db
Singh <eltigre.svo@gmail.com>
2020-01-22 17:45:55 +0100

    Removed script from home.php because it doesn't belong there

--- home.php
+++ home.php
@@ -77,24 +77,7 @@
     }
   </style>
 </head>
-<!--Script to show temperature-->
-<script type="text/javascript">
-  function showTemp() {{
-      var xmlhttp = new XMLHttpRequest();
-      xmlhttp.onreadystatechange = function() {
-          if (this.readyState == 4 && this.status == 200) {
-              document.getElementById("content").innerHTML = this.responseText;
-          }
-      }
-      xmlhttp.open("GET", "ajax_temperature.php", true);
-      xmlhttp.send();
-      }
-  }
 
-  window.setInterval(function() {
-    	showTemp()
-  }, 1000);
-</script>
 <!--Website body-->
 <body>
   <!--The navigation bar on the top of the webpage-->
