5c7a0b512ef2b8362d213455242adb1c743d8446
Hessel Jager <h.f.jager@st.hanze.nl>
2020-01-22 18:18:49 +0100

    centreren gefixt

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 		}
 	}
 ?>
-<!--Initiate webpage-->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -35,6 +35,8 @@
     /*set style for body*/
     body {
       height: 100%;
+    }
+    body {
       display: -ms-flexbox;
       display: flex;
       -ms-flex-align: center;
@@ -50,11 +52,11 @@
       padding: 15px;
       margin: auto;
     }
-    /*set style for signin checkbox*/
+    /*set style for sin
     .form-signin .checkbox {
       font-weight: 400;
     }
-    /*set style for signin control*/
+
     .form-signin .form-control {
       position: relative;
       box-sizing: border-box;
@@ -62,23 +64,23 @@
       padding: 10px;
       font-size: 16px;
     }
-    /*set style for signin control*/
+
     .form-signin .form-control:focus {
       z-index: 2;
     }
-    /*set style for username*/
+
     .form-signin input[type="text"] {
       margin-bottom: -1px;
       border-bottom-right-radius: 0;
       border-bottom-left-radius: 0;
     }
-    /*set style for password*/
+
     .form-signin input[type="password"] {
       margin-bottom: 10px;
       border-top-left-radius: 0;
       border-top-right-radius: 0;
     }
-    /*set style for submit button*/
+
     .form-signin input[type="submit"] {
       background-color: #184893;
       margin-bottom: 10px;
@@ -88,49 +90,53 @@
       -webkit-transition-duration: 0.2s; /* Safari */
       transition-duration: 0.2s;
     }
-    /*set style for submit buttom hover*/
+
     .form-signin input[type="submit"]:hover {
       background-color: #DC292A;
     }
-    /*set style for */
-    .bd-placeholder-img {
-      font-size: 1.125rem;
-      text-anchor: middle;
-      -webkit-user-select: none;
-      -moz-user-select: none;
-      -ms-user-select: none;
-      user-select: none;
-    }
-    /*set style for media*/
-    @media (min-width: 768px) {
-      .bd-placeholder-img-lg {
-        font-size: 3.5rem;
+
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
       }
-    }
-  </style>
-  <!-- Custom styles for this template -->
-  <link href="signin.css" rel="stylesheet">
-</head>
-<!--Webpage body-->
-<body class="text-center">
-  <!--Sign in form-->
-  <form class="form-signin" method="post" action="">
-    <!--GIEPA logo-->
-    <img class="mb-4" src="https://www.giepa.gm/sites/default/files/logo-giepa.png" alt="" style="width: 80%;">
-    <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-    <h6 class="h6 mb-6 font-weight-normal text-danger"><?php echo $error; ?></h6>
-    <!--Username input-->
-    <label for="inputEmail" class="sr-only">Username</label>
-    <input name="username" type="text" id="inputEmail" class="form-control" placeholder="Username" required autofocus>
-    <!--Password input-->
-    <label for="inputPassword" class="sr-only">Password</label>
-    <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-    <!--Checkbox divider-->
-    <div class="checkbox mb-3"></div>
-    <!--Submit button-->
-    <input type='submit' value="Sign in" name="submit" class="btn btn-lg btn-primary btn-block" type="submit">
-    <!--Copyright bottom text-->
-    <p class="mt-5 mb-3 text-muted">&copy; Copyright <?php echo date("Y"); ?></p>
-  </form>
-</body>
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="signin.css" rel="stylesheet">
+  </head>
+  <body class="text-center">
+    <form class="form-signin" method="post" action="">
+      <img class="mb-4" src="https://www.giepa.gm/sites/default/files/logo-giepa.png" alt="" style="width: 80%;">
+
+      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+
+      <h6 class="h6 mb-6 font-weight-normal text-danger"><?php echo $error; ?></h6>
+
+      <label for="inputEmail" class="sr-only">Username</label>
+
+      <input name="username" type="text" id="inputEmail" class="form-control" placeholder="Username" required autofocus>
+
+      <label for="inputPassword" class="sr-only">Password</label>
+
+      <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+	  
+      <div class="checkbox mb-3"></div>
+
+      <input type='submit' value="Sign in" name="submit" class="btn btn-lg btn-primary btn-block" type="submit">
+
+      <p class="mt-5 mb-3 text-muted">&copy; Copyright <?php echo date("Y"); ?></p>
+    </form>
+  </body>
 </html>
+<?php 
+	
+?>
