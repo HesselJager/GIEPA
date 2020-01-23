a07fd046153a98024a38a6e4969765b4e27b9591
Singh <eltigre.svo@gmail.com>
2020-01-23 12:29:12 +0100

    Added showTable function

--- view_station.php
+++ view_station.php
@@ -233,6 +233,18 @@
 	  xmlhttp.open("GET", "ajax_wind_speed.php"+getParam, true);
 	  xmlhttp.send();
 	}
+	   //function to show table with weatherdata
+	function showTable(){
+	  {
+	    var xmlhttp = new XMLHttpRequest();
+	    xmlhttp.onreadystatechange = function() {
+	      if (this.readyState == 4 && this.status == 200) 
+          addData(window.myLine, '', this.responseText);
+	    }
+	  }
+	  xmlhttp.open("GET", "ajax_table.php"+getParam, true);
+	  xmlhttp.send();
+	}
 	
 	
 	
