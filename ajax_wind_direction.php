7d998a931a8b19ede14f4728c3eb9004f2e9f12f
Singh <eltigre.svo@gmail.com>
2020-01-25 18:16:53 +0100

    Error handled in ajax_wind_direction when there is no data yet

--- ajax_wind_direction.php
+++ ajax_wind_direction.php
@@ -4,5 +4,7 @@
 	
 	$stationdata=$_SESSION["measurements"][intval($_GET["station"])];
 	//ouput the latest dataset of the full data
+	if (array_key_exists(sizeof($stationdata)-1,$stationdata)){
 	echo "Current wind direction: " . wnddir_to_words($stationdata[sizeof($stationdata)-1]->wnddir);
+	}
 ?>
