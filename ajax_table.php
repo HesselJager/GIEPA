c09fdb016d8fccc58246f5872f427f473d747b47
Jojahoka <jojahoka@gmail.com>
2020-01-26 19:32:09 +0100

    Reversed table, most recent first

--- ajax_table.php
+++ ajax_table.php
@@ -6,15 +6,14 @@
 	//output the latest dataset of the full data
 	$output = "<table id='wind_table' border='1px'><thead><th>Date and Time</th><th>   Windspeed</th><th>Winddirection</th></thead>";
 	$output .= '<tbody onscroll="pauseTable()">';
+	$row = "";
 	for($measurementIndex = 0; $measurementIndex < sizeof($stationdata); $measurementIndex++){
 		$wndspd = $stationdata[$measurementIndex]->wdsp;
 		$wnddir = $stationdata[$measurementIndex]->wnddir;
 		$time = $stationdata[$measurementIndex]->date_and_time;
 		$time = $time->format('d/m/Y H:i:s');
-		$output .= "<tr><td>" . $time . "</td>";
-		$output .= "<td>" . $wndspd . "&nbsp; km/h</td>";
-		$output .= "<td>" . wnddir_to_words($wnddir) . "&nbsp; (" . $wnddir . "&#176;)</td></tr>";
+		$row = "<tr><td>" . $time . "</td><td>" . $wndspd . "&nbsp; km/h</td><td>" . wnddir_to_words($wnddir) . "&nbsp; (" . $wnddir . "&#176;)</td></tr>" . $row;
 	}
-	$output .= "</tbody></table>";
+	$output .= $row . "</tbody></table>";
 	echo $output;
 ?>
--- ajax_table_temp.php
+++ ajax_table_temp.php
@@ -6,13 +6,13 @@
 	//output the latest dataset of the full data
 	$output = "<table id='temp_table' border='1px'><thead><th>Date and Time</th><th>Temperature</th></thead>";
 	$output .= '<tbody onscroll="pauseTable()">';
+	$row = "";
 	for($measurementIndex = 0; $measurementIndex < sizeof($stationdata); $measurementIndex++){
 		$temp = $stationdata[$measurementIndex]->temp;
 		$time = $stationdata[$measurementIndex]->date_and_time;
 		$time = $time->format('d/m/Y H:i:s');
-		$output .= "<tr><td>" . $time . "</td>";
-		$output .= "<td>" . $temp . "&#8451;</td></tr>";
+		$row = "<tr><td>" . $time . "</td><td>" . $temp . "&#8451;</td></tr>" . $row;
 	}
-	$output .= "</tbody></table>";
+	$output .= $row . "</tbody></table>";
 	echo $output;
 ?>
