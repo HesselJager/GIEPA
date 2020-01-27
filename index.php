0334781f6d20cc5468d91fcbd91004bc32a5b3e9
Singh <eltigre.svo@gmail.com>
2020-01-27 12:13:17 +0100

    Tab icon added

--- index.php
+++ index.php
@@ -19,6 +19,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <link rel="icon" href="giepa.png">
   <!--Opening code for bootstrap and login-->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
