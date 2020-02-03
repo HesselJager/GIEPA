ffb90c0369ce9650922b5e48a5f2535516ba58e6
Hessel Jager <h.f.jager@st.hanze.nl>
2020-02-03 09:32:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <link rel="icon" href="giepa.png">
+  <link rel="icon" href="./images/giepa.png">
   <!--Opening code for bootstrap and login-->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
